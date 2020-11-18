3f19a884f29624137e9cc81f1a2245ff381a05a0
Adam Koksal <49299708+adamkoksal@users.noreply.github.com>
2020-11-17 20:44:53 -0600

    feat: make columns draggable

--- client/src/components/Table.vue
+++ client/src/components/Table.vue
@@ -3,8 +3,23 @@
     <thead>
       <tr>
         <th scope="col" style="width: 6.7%">#</th>
-        <th scope="col" v-for="field in fieldsShown" :key="field">
-          {{ field }}
+        <th
+          scope="col"
+          @dragstart="dragField = field"
+          @dragend="move"
+          @dblclick="removeField(field)"
+          id="move"
+          v-for="field in fieldsShown"
+          :key="field"
+          draggable
+        >
+          <div class="container">
+            <div class="dropzone" @dragover="dragover(field, 1, $event)" />
+            <div class="col-name" @dragover="dragover(field, 1, $event)">
+              {{ field }}
+            </div>
+            <div class="dropzone" @dragover="dragover(field, 0, $event)" />
+          </div>
         </th>
       </tr>
     </thead>
@@ -16,9 +31,6 @@
         </td>
       </tr>
     </tbody>
-    <input v-model="from" />
-    <input v-model="to" />
-    <button @click="some">click</button>
   </table>
 </template>
 
@@ -32,12 +44,32 @@ export default {
     return {
       fieldsShown: ["FirstName", "LastName"],
       page: 1,
-      from: "", to: ""
+      dragField: null,
+      dragPosition: null,
+      dragTo: null,
     };
   },
   methods: {
     move() {
-      this.fieldsShown.splice(this.to, 0, this.fieldsShown.splice(this.from, 1)[0]);
+      const from = this.findIndex(this.dragField);
+      let to = this.findIndex(this.dragTo);
+      if (!this.dragPosition) ++to;
+      this.fieldsShown.splice(to, 0, this.fieldsShown.splice(from, 1)[0]);
+    },
+    removeField(field) {
+      const noDelete = ["FirstName", "LastName"];
+      if (!noDelete.includes(field)) {
+        const rmItem = this.findIndex(field);
+        this.fieldsShown.splice(rmItem, 1);
+      }
+    },
+    dragover(field, position, $event) {
+      $event.preventDefault();
+      this.dragPosition = position;
+      this.dragTo = field;
+    },
+    findIndex(field) {
+      return this.fieldsShown.findIndex((element) => element == field);
     },
   },
   created() {
@@ -56,4 +88,30 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+#move {
+  cursor: move;
+  padding: 0;
+}
+
+.dropzone {
+  margin: 0px;
+  padding: 0px;
+  width: 130px;
+  height: 20px;
+}
+
+.container {
+  margin: 0px;
+  display: table;
+  height: 50px;
+}
+
+.dropzone {
+  display: table-cell;
+}
+
+.col-name {
+  padding-top: 12px;
+}
+</style>
