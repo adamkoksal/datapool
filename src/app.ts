feaf63ba28cf6c776919066388b69a67e4309707
Adam Koksal <49299708+adamkoksal@users.noreply.github.com>
2021-01-02 22:31:40 -0600

    refactor: create public folder

--- src/app.ts
+++ src/app.ts
@@ -13,7 +13,7 @@ const cors = require("cors");
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "../dist/public")));
+app.use(express.static(path.join(__dirname, "../public")));
 
 server.applyMiddleware({ app, path: `/graphql` });
 
